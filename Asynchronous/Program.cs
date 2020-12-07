cc39e1844d65fa00958dacd8db4c39e2db276433
olgamezhova <o.mezhova@dodopizza.com>
2020-12-07 15:35:24 +0300

    Implement MoveCarSync async method

---
+++ Asynchronous/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Threading;
+
+namespace Asynchronous
+{
+    class Program
+    {
+        public static bool isActionCalled;
+
+        static void Main(string[] args)
+        {
+            var isCarArrived = false;
+            MoveCarSync();
+            MoveCarSync();
+        }
+
+        static void MoveCarSync()
+        {
+            MoveCar(MoveCar);
+
+            while (!isActionCalled)
+            {
+                Thread.Sleep(1000);
+            }
+
+            isActionCalled = false;
+        }
+
+        static void MoveCar(Action callback)
+        {
+            Timer t = null;
+            t = new Timer(_ =>
+            {
+                callback();
+                t?.Dispose();
+            }, null, 1000, Int32.MaxValue);
+        }
+
+        static void MoveCar()
+        {
+            Console.WriteLine("Car arrived");
+            isActionCalled = true;
+        }
+    }
+}
